529e5cc97615a58a949abe589d601019b555ee3e
Nate Abele <nate.abele@gmail.com>
2022-03-07 17:08:54 -0500

    Simple HTTP server

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-
 FROM openjdk:11
 WORKDIR /app
+EXPOSE 8000
 ADD target/hello-world-0.1.0.jar /app/hello-world.jar
 ENTRYPOINT ["java", "-jar", "hello-world.jar"]
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,13 +1,36 @@
 package hello;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-      LocalTime currentTime = new LocalTime();
-		  System.out.println("Hello world!! The current local time is: " + currentTime);
 
-     
+  public static void main(String[] args) throws Exception {
+    Integer port = 8000;
+    HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+    server.createContext("/", new MyHandler());
+    server.setExecutor(null); // creates a default executor
+    server.start();
+    System.out.println("Started demo server on port: " + port);
+  }
+
+  static class MyHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+      LocalTime currentTime = new LocalTime();
+      String response = "Hello world. The current local time is: " + currentTime;
+      t.sendResponseHeaders(200, response.length());
+
+      OutputStream os = t.getResponseBody();
+      os.write(response.getBytes());
+      os.close();
     }
+  }
 }
- 
